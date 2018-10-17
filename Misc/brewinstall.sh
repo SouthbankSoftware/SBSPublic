8cf5f27c51d736525774033ac24e99ca8527aba0
Guy Harrison <guy.a.harrison@gmail.com>
2018-10-17 17:38:56 +1100

    latest brew changes

--- Misc/brewinstall.sh
+++ Misc/brewinstall.sh
@@ -3,9 +3,13 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew install caskroom/cask/brew-cask
 brew install  emacs gdbm git gmp gnutls icu4c jasper jpeg libtasn1 libtool lynx maven scala trash thefuck
 brew install mongodb mysql nettle node npm nvm openssl perl postgresql python readline socat sqlite wget
-brew install apache-drill gcc docker maven
+brew install apache-drill gcc docker maven jq readline ruby sqlite htop 
 brew cask install adobe-reader iterm2 skype java
 brew cask install atom docker dropbox evernote google-chrome intellij-idea slack spectacle vagrant virtualbox
-brew cask install adobe-creative-cloud sketch visual-studio-code balsamiq-mockups yarn golang godep
+brew cask install adobe-creative-cloud sketch visual-studio-code balsamiq-mockups zeplin gitkraken dbkoda 
+brew cask install postman tigervnc-viewer zoomus
+brew install yarn golang godep
+brew cask install minikube numi
+brew install kubecfg kubernetes-cli 
 
 
