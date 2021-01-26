63d8555dda8ef305e5aa397c63fabf1c19f1dfb0
Guy Harrison <guy@southbanksoftware.com>
2021-01-27 09:25:09 +1100

    Update brewinstall.sh

--- Misc/brewinstall.sh
+++ Misc/brewinstall.sh
@@ -1,17 +1,144 @@
 xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install caskroom/cask/brew-cask
-brew install  emacs gdbm git gmp gnutls icu4c jasper jpeg libtasn1 libtool lynx maven scala trash thefuck
-brew install mongodb mysql nettle node npm nvm openssl perl postgresql python readline socat sqlite wget
-brew install apache-drill gcc docker maven jq readline ruby sqlite htop 
-brew cask install adobe-reader iterm2 skype java
-brew cask install atom docker dropbox evernote google-chrome intellij-idea slack spectacle vagrant virtualbox
-brew cask install adobe-creative-cloud sketch visual-studio-code balsamiq-mockups zeplin gitkraken dbkoda 
-brew cask install postman tigervnc-viewer zoomus
-brew install yarn golang godep
-brew cask install minikube numi
-brew install kubecfg kubernetes-cli kubernetes-helm watch 
-brew tap mongodb/brew
+brew install adns
+brew install adwaita-icon-theme
+brew install apache-drill
+brew install asciinema
+brew install atk
+brew install autoconf
+brew install automake
+brew install azure-cli
+brew install bat
+brew install bdw-gc
+brew install c-ares
+brew install cairo
+brew install calc
+brew install coreutils
+brew install doitlive
+brew install emacs
+brew install ethereum
+brew install evans
+brew install flux
+brew install fontconfig
+brew install freetype
+brew install fribidi
+brew install gcc
+brew install gd
+brew install gdbm
+brew install gdk-pixbuf
+brew install gettext
+brew install git
+brew install glib
+brew install gmp
+brew install gnupg
+brew install gnutls
+brew install go
+brew install gobject-introspection
+brew install gpgme
+brew install graphite2
+brew install graphviz
+brew install grpcurl
+brew install gsettings-desktop-schemas
+brew install gtk+3
+brew install gts
+brew install guile
+brew install harfbuzz
+brew install helm
+brew install hicolor-icon-theme
+brew install htop
+brew install icu4c
+brew install isl
+brew install istioctl
+brew install jansson
+brew install jasper
+brew install jemalloc
+brew install jpeg
+brew install jq
+brew install k9s
+brew install krb5
+brew install kubernetes-cli
+brew install kubernetes-helm
+brew install lazydocker
+brew install libassuan
+brew install libepoxy
+brew install libev
+brew install libevent
+brew install libffi
+brew install libgcrypt
+brew install libgpg-error
+brew install libidn2
+brew install libksba
+brew install libmpc
+brew install libpng
+brew install librsvg
+brew install libtasn1
+brew install libtiff
+brew install libtool
+brew install libunistring
+brew install libusb
+brew install libyaml
+brew install lynx
+brew install lzo
+brew install makensis
+brew install maven
+brew install minikube
 brew install mongodb-community
-
-
+brew install mongosh
+brew install mpfr
+brew install msodbcsql
+brew install msodbcsql17
+brew install mssql-tools
+brew install mutt
+brew install mysql
+brew install ncurses
+brew install netpbm
+brew install nettle
+brew install nghttp2
+brew install npth
+brew install numpy
+brew install nvm
+brew install oniguruma
+brew install openblas
+brew install openjdk@11
+brew install openssl@1.1
+brew install p11-kit
+brew install p7zip
+brew install pango
+brew install pcre
+brew install pcre2
+brew install perl
+brew install pig
+brew install pinentry
+brew install pixman
+brew install pkg-config
+brew install postgresql
+brew install postgresql@11
+brew install protobuf
+brew install py3cairo
+brew install pygobject3
+brew install python@3.9
+brew install readline
+brew install ruby
+brew install scala
+brew install scons
+brew install socat
+brew install sops
+brew install spark
+brew install sphinx-doc
+brew install sqlite
+brew install tcl-tk
+brew install tcping
+brew install unbound
+brew install unixodbc
+brew install velero
+brew install vsftpd
+brew install watch
+brew install webp
+brew install webpack
+brew install wget
+brew install xdot
+brew install xz
+brew install yarn
+brew install yq
+brew install cask zoom wireshark visual-studio-code virtualbox dbkoda screenflow skype slack whatsapp telegram evernote parallels postmane dropbox docker google-chrome spectacle minikube zeplin gitkraken
